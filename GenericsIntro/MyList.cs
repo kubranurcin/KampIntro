324a5e6a7ad1d47a964e9115f5ac5274dc763b6d
kubranurcin <kubra.nur.96@hotmail.com>
2021-01-23 20:23:11 +0300

    20 Ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;//gecici dizinin referansı = itemin referansi sonra da;
+            items = new T[items.Length+1]; //eleman sayisini arttirdim
+            for (int i = 0; i < tempArray.Length; i++)//eski elemanlara yeniden kavusmak icin bunu yaptik
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
