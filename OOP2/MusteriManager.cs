a8950d724ca911444fa1100853453547f01d69b6
kubranurcin <kubra.nur.96@hotmail.com>
2021-01-27 20:44:23 +0300

    23 ocak ders sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //manager siniflari is siniflaridir
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if(musteri)
+            //{
+
+            //}
+
+        }
+    }
+}
